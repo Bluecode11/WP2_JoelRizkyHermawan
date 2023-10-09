0a3243a2839c8c3d0dd1b055f1d959d8bce65e97
Bluecode11 <joelrizkyhermawan201@gmail.com>
2023-10-09 16:11:58 +0700

    membuat template sederhana dan dinamis dan setting base_url()

---
+++ pustaka-booking/application/views/v_about.php
@@ -0,0 +1,25 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Joel</li>
+        <li>Nama Belakang : Hermawan</li>
+    </ul>
+
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Fajar Baru Selatan No.64 Cengkareng Timur </li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Koyawas</li>
+    </ul>
+
+    <h4>Olahraga Favorit</h4>
+    <ul type="square">
+        <li>Berenang</li>
+        <li>Main Game</li>
+    </ul>
+</section>
